9e679fb8da8d0929ff5beb98d02a7d56da75b21b
YaroShkvorets <shkvorets@gmail.com>
2022-02-08 14:23:15 -0500

    validate read_transaction()

--- claim.pomelo.cpp
+++ claim.pomelo.cpp
@@ -184,5 +184,6 @@ checksum256 claimpomelo::get_trx_id()
     size_t size = transaction_size();
     char buf[size];
     size_t read = read_transaction( buf, size );
+    check( size == read, "claimpomelo::get_trx_id: read_transaction failed");
     return sha256( buf, read );
 }
